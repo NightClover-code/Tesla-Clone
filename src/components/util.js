07315f4b9f60177c85727171fd61a8a9e33be793
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2020-11-27 18:49:44 +0100

    added util.js containing website information

---
+++ src/components/util.js
@@ -0,0 +1,45 @@
+const information = () => {
+  return [
+    {
+      title: 'Only $1.49/Watt for Solar on Existing Roofs',
+      detail: 'Lowest Cost in America - Money-back guarantee',
+      buttonLeft: 'Order now',
+      buttonRight: 'Learn more',
+    },
+    {
+      title: 'Model S',
+      detail: 'Starting at $69,420',
+      buttonLeft: 'Custom order',
+      buttonRight: 'Existing inventory',
+    },
+    {
+      title: 'Model 3',
+      detail: 'Order Online for Touchless Delivery',
+      buttonLeft: 'Custom order',
+      buttonRight: 'Existing inventory',
+    },
+    {
+      title: 'Model X',
+      detail: 'Order Online for Touchless Delivery',
+      buttonLeft: 'Custom order',
+      buttonRight: 'Existing inventory',
+    },
+    {
+      title: 'Model Y',
+      detail: 'Order Online for Touchless Delivery',
+      buttonLeft: 'Custom order',
+      buttonRight: 'Learn more',
+    },
+    {
+      title: 'Solar for New Roofs',
+      detail: 'Solar Roof Costs Less Than a New Roof Plus Solar Panels',
+      buttonLeft: 'Order now',
+      buttonRight: 'Learn more',
+    },
+    {
+      title: 'Accessories',
+      button: 'Shop now',
+    },
+  ];
+};
+export default information();
